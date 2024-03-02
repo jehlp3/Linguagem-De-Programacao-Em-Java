d0559bdc2879302bf6d8103703470901d85d48d4
jehlp3 <jessicalacerdap@hotmail.com>
2024-03-02 14:33:15 -0300

    Java Intermediario

--- ArrayTrabalhando/src/TrabalhandoArray/TrabalhandoArray.java
+++ ArrayTrabalhando/src/TrabalhandoArray/TrabalhandoArray.java
@@ -3,36 +3,33 @@ package TrabalhandoArray;
 import javax.swing.JOptionPane;
 
 public class TrabalhandoArray {
+	public static void main (String[] args) {
+		int[] numeros = new int[10];
+		
+		String [] paises = new String[4];
+		
+		/*
+		for (int i = 0; i < 4; i++) {
+			paises[i] = JOptionPane.showInputDialog("Informe um país: ");
+			//variavel i ficou dentro do escopo, e foi finalizada 
+		}
+		for (String listaPaises:paises) {
+			System.out.println(listaPaises);
+		}*/
+		
+		for (int i = 0; i < 10; i++) {
+			numeros[i] = Integer.parseInt( JOptionPane.showInputDialog("informe um número: "));
+		}
+		for (Integer listaNumeros : numeros) {
+			System.out.println(listaNumeros);
+		}
+		int novoValor = 11;
+		numeros[8] = novoValor+4;
+		for (Integer listaNumeros : numeros) {
+			System.out.println(listaNumeros);
+		}
+		
+	} 
 	
-	public static void main (String[]args) {
-	
-	String [] paises = new String[5];
-	
-	paises [0]="Brasil";
-	paises [1]="Alemanha";
-	paises [2]="Itália";
-	paises [3]="Áustria";
-	paises [4]="Polônia";
-	
-	for (String lista: paises) {
-		System.out.println(lista);
-	}	
-	
-	int [] numeros = new int [10];
-	
-	for (int i = 0; i < numeros.length; i++) {
-		numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Informe um número: "));
-	}
-	
-	System.out.println("Após a entrada de dados: ");
-	for (int listaNumeros : numeros) {
-		System.out.println(listaNumeros + " ");
-	}
-	
-	numeros[8] = numeros[7]+4;
-	System.out.println("\nApós a alteração do valor da posição 8");
-	
-	for (int listaNumeros: numeros) {
-		System.out.println(listaNumeros + " ");
-	}
+
 }
